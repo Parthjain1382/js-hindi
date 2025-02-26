2b9e63256421edf80f5746754278b7e7973153a6
pranay jain <pranayjain1382.com>
2025-02-26 15:00:38 +0530

    cnversionDataType

---
+++ 02_dataTypes.js
@@ -0,0 +1,12 @@
+//Data Types in Js
+/** There are seven primitive data types in js 
+ * Number
+ * BigInt
+ * String
+ * Boolean
+ * Symbol
+ * null  type: Object
+ * undefined type: undefined
+ */
+
+alert(2+2)
---
+++ 03_conversionDataType.js
@@ -0,0 +1,19 @@
+//dataType conversion
+
+let score = "hitest"
+console.log("score is and type",score, typeof(score)) 
+score = Number(score)
+console.log("score is and type",score, typeof(score)) 
+
+
+
+//"33"  ->  33
+//"ab" -> NaN
+//true  -> 1 and false  -> 0
+
+
+// string conversion to numbers 
+
+console.log("1" + 2)   //12
+console.log(1 + 2 + "2")  //32
+ 
